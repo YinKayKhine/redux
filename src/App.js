26ec82b381807bfa33560b49cdfd39238bc99a9d
kay-ab <kay@aluto-benli.com>
2020-01-12 23:36:48 +0630

    Init repo

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Articles from './containers/Articles';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <Articles />
+}
+
+export default App;
---
+++ src/containers/Articles.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react"
+import { connect } from "react-redux"
+import Article from "../components/Article/Article"
+import AddArticle from "../components/AddArticle/AddArticle"
+import * as actionTypes from "../store/actionTypes"
+
+// const Articles = () => {
+//   const [articles, setArticles] = useState([
+//     { id: 1, title: "post 1", body: "Quisque cursus, metus vitae pharetra" },
+//     { id: 2, title: "post 2", body: "Quisque cursus, metus vitae pharetra" },
+//   ])
+//   const saveArticle = e => {
+//     e.preventDefault()
+//     // the logic will be updated later
+//   }
+
+//   return (
+//     <div>
+//       <AddArticle saveArticle={saveArticle} />
+//       {articles.map(article => (
+//         <Article key={article.id} article={article} />
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default Articles
+const Articles = ({ articles, saveArticle }) => (
+    <div>
+      <AddArticle saveArticle={saveArticle} />
+      {articles.map(article => (
+        <Article key={article.id} article={article} />
+      ))}
+    </div>
+  )
+  
+  const mapStateToProps = state => {
+    return {
+      articles: state.articles,
+    }
+  }
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      saveArticle: article =>
+        dispatch({ type: actionTypes.ADD_ARTICLE, articleData: { article } }),
+    }
+  }
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Articles)
